6ac1d72e932b5238f7132b25e08c91da03eb0dbb
Rvalenzuela30 <RLValenzuela30@gmail.com>
2025-05-22 23:11:52 -0400

    Frontend creation with Navbar

---
+++ client/src/pages/Login.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Login() {
+  return (
+    <div>
+      <h2>Login or Create Account</h2>
+      <p>Choose which you'd like to do.</p>
+    </div>
+  );
+}
+
+export default Login;
---
+++ client/src/pages/Products.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Products() {
+  return (
+    <div>
+      <h2>All Products</h2>
+      <p>Testing text to show all of the products on the page</p>
+    </div>
+  );
+}
+
+export default Products;
