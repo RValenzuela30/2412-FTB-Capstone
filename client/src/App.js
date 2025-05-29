4050423ef5f289b854d659a4c3804821f8f849bf
Emma Grace <emmalaslo@gmail.com>
2025-05-28 22:22:25 -0400

    app.js changed to use components

--- client/src/App.js
+++ client/src/App.js
@@ -1,57 +1,15 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.js";
 import Products from "./pages/Products.js";
 import Orders from "./pages/Orders.js";
 import Login from "./pages/Login.js";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <Router>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light shadow-sm">
-        <div className="container-fluid">
-          <Link className="navbar-brand fw-bold" to="/">
-            🐾 Pet Store
-          </Link>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon" />
-          </button>
-
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav ms-auto">
-              <li className="nav-item">
-                <Link className="nav-link" to="/">
-                  Home
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/products">
-                  Products
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/orders">
-                  My Orders
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/login">
-                  Login/Signup
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-
+      <Navbar /> {/* Use new component */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<Products />} />
